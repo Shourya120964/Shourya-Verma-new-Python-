0c4a5636abec09b55ea435e55abf7caf371aeb2c
shourya <shourya2008.verma@gmail.com>
2022-06-13 15:08:07 +0530

    python

---
+++ hello.py
@@ -0,0 +1,5 @@
+a = "Shourya Verma"
+b = 879
+c = 8.99
+print(b+c)
+print(b<c)
---
+++ try code.py
@@ -0,0 +1,9 @@
+a = input("Enter your name:-")
+print(a)
+b = input("How old are you:-")
+print(b)
+b = int
+c = input("What are you doing in home:-")
+print(c)
+d = input("Enter your E-mail ID :- ")
+print(d)
